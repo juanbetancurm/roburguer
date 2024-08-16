49fd5ecaef12d53fd25d5be93a7af856f0af1709
juanjbetancur <juan.jbetancur852@gmail.com>
2024-08-16 18:11:49 -0500

    First Upload Created Project

---
+++ src/main/java/com/rockburger/rocburapp/RocburappApplication.java
@@ -0,0 +1,13 @@
+package com.rockburger.rocburapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocburappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocburappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rockburger/rocburapp/RocburappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockburger.rocburapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocburappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
